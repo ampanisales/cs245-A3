b88d021f700c0deb80b2d33cb16dd8fab6e3d2c9
ampanisales <31353791+ampanisales@users.noreply.github.com>
2017-11-27 18:20:01 -0800

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -3,31 +3,64 @@ import java.util.Iterator;
 import java.util.LinkedList;
 
 /**
- * Class Purpose: 
+ * Class Purpose: Holds all of the names of 
+ * actors that were read from the the 
+ * 'tmdb_5000_credits.csv' file
  * 
  * @author Anthony Panisales
  */
 
 public class Graph {
 	
-	int[][] edges;
-	
-	public Graph(int vertices) {
-		edges = new int[vertices][vertices];
+	class Vertex {
+		
+		String value; 
+		LinkedList<Vertex> connections;
+		
+		public Vertex(String newVal) {
+			value = newVal;
+			connections = new LinkedList<Vertex>();
+		}
 	}
 
-	public void addEdge(int v1, int v2) {
-		edges[v1][v2] = 1;
-		edges[v2][v1] = 1;
+	Vertex[] vertices;
+	
+	public Graph(int n) {
+		vertices = new Vertex[n];
+		for (int i = 0; i < vertices.length; i++)
+			vertices[i] = new Vertex(null);
 	}
 	
-	private LinkedList<Integer> getNeighbors(int vertex) {
-		LinkedList<Integer> list = new LinkedList<Integer>();
-		for (int i = 0; i < edges.length; i++) {
-			if (edges[vertex][i] == 1)
-				list.add(new Integer(i));
+	/**
+	 * Function Purpose: Searches for the vertex in 
+	 * 'vertices' with a specific actor's name. Returns
+	 * the index of that vertex in 'vertices' if located.
+	 * Returns -1 otherwise.
+	 */
+	public int search(Vertex[] arr, String target) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i].value == null)
+				return -1;
+			else if (arr[i].value.equals(target))
+				return i;
 		}
-		return list;
+		return -1;
+	}
+	
+	public void addEdge(int actorAVertex, int actorBVertex) {
+		Vertex v1 = vertices[actorAVertex];
+		Vertex v2 = vertices[actorBVertex];
+		v1.connections.add(v2);
+		v2.connections.add(v1);
+	}
+	
+	/**
+	 * Function Purpose: Used in the findPath() method.
+	 * Returns the LinkedList of connections with other vertices
+	 * that a vertex in 'vertices' has.
+	 */
+	private LinkedList<Vertex> getNeighbors(int index) {
+		return vertices[index].connections;
 	}
 	
 //	public void bfs() {
@@ -57,33 +90,36 @@ public class Graph {
 //		}
 //	}
 	
-	
+	/**
+	 * Function Purpose: Finds the shortest path between two vertices
+	 * and returns the path as an ArrayList.
+	 */
 	//IS IT OK FOR THIS TO BE RECURSIVE?
-	public ArrayList<Integer> findPath(int vertexA, int vertexB) {
-		boolean[] visited = new boolean[edges.length];
-		return findPath(vertexA, vertexB, visited);
+	public ArrayList<String> findPath(int indexA, int indexB) {
+		boolean[] visited = new boolean[vertices.length];
+		return findPath(indexA, indexB, visited);
 	}
 	
-	private ArrayList<Integer> findPath(int vertexA, int vertexB, boolean[] visited) {
+	private ArrayList<String> findPath(int indexA, int indexB, boolean[] visited) {
 		ArrayQueue q = new ArrayQueue();
-		q.enqueue(vertexA);
-		ArrayList<Integer> path = new ArrayList<Integer>();
-		if (vertexA == vertexB) {
-			path.add(vertexA);
+		q.enqueue(indexA);
+		ArrayList<String> path = new ArrayList<String>();
+		if (indexA == indexB) {
+			path.add(vertices[indexA].value);
 			return path;
 		}
 		while (!q.empty()) {
 			int v = (Integer) q.dequeue();
 			visited[v] = true;
-			Iterator<Integer> it = getNeighbors(v).iterator();
+			Iterator<Vertex> it = getNeighbors(v).iterator();
 			while (it.hasNext()) {
-				int u = it.next();
+				int u = search(vertices, it.next().value);
 				if (!visited[u]) {
 					q.enqueue(u);
 					visited[u] = true;
-					if (u == vertexB) {
-						path.add(u);
-						path.addAll(findPath(vertexA, v));
+					if (u == indexB) {
+						path.add(vertices[u].value);
+						path.addAll(findPath(indexA, v));
 						return path;
 					}
 				}
